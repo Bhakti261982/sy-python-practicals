996044075d962ce1f06f297ba44ea242fe3ead60
Bhakti261982 <bhaktinikam007@gmail.com>
2026-07-30 13:20:42 +0530

    Add files via upload

--- while loop statement.py
+++ while loop statement.py
@@ -30,5 +30,15 @@ print("loop terminated")'''
 '''for i in range(1,6):
     print("*")'''
 
-for i in range(1,6):
-    print("*"*i)
+'''for i in range(1,6):
+    print("*"*i)'''
+
+'''for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()'''
+
+'''for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()'''
