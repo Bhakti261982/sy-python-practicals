508e2e974ac34cd984230f3caf39e34435ba1ff9
Bhakti261982 <bhaktinikam007@gmail.com>
2026-07-30 12:47:00 +0530

    Add files via upload

---
+++ while loop statement.py
@@ -0,0 +1,34 @@
+'''n=int(input("Enter your number"))
+while n!=0:
+    d=n%10;
+    print(d,end=" ")
+    n//=10'''
+
+'''fruit=("apple","banana","orange")
+for fruit in fruit:
+    print(fruit)'''
+
+'''sum=0
+for val in range (1,10):
+    sum=sum+val
+print(sum,"is the mumber of 1st 5natural numbers")'''
+
+'''print("Hello learner")'''
+
+'''num=1
+while num<5:
+    print(num)'''
+
+'''count=1
+while count <=5:
+    if count == 3:
+        break;
+print (count)
+count =+ 1
+print("loop terminated")'''
+
+'''for i in range(1,6):
+    print("*")'''
+
+for i in range(1,6):
+    print("*"*i)
