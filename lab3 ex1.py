a530c8c57f3f0b641d79c020781e0661d7de02d1
Bhakti261982 <bhaktinikam007@gmail.com>
2026-08-13 13:05:00 +0530

    Add files via upload

---
+++ lab3 ex1.py
@@ -0,0 +1,7 @@
+age= int(input("Enter your age :"))
+income=float(input("Enter your income :"))
+
+if age < 25 and income < 300000 :
+    print("congratulation! .You are qualified for education scolarship scheme. ")
+else:
+    print("You are not qualified for education scolarship scheme. ")
