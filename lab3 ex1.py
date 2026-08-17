37cfb28b4fe3ac49d37ed17e98724f222155a33f
Bhakti261982 <bhaktinikam007@gmail.com>
2026-08-17 10:43:39 +0530

    Add files via upload

--- lab3 ex1.py
+++ lab3 ex1.py
@@ -1,7 +1,8 @@
+name=input("Enter your name :")
 age= int(input("Enter your age :"))
 income=float(input("Enter your income :"))
-
-if age < 25 and income < 300000 :
-    print("congratulation! .You are qualified for education scolarship scheme. ")
+cast=input("Enter your cast ( sc/st/mt/obc):")
+if age < 25 and income < 300000 and cast in ["sc","st","nt","obc"]:
+    print("congratulation! .You are qualified for education scolarship scheme. ")  
 else:
     print("You are not qualified for education scolarship scheme. ")
