adba1414b603e7afdbda8f4e5cdb9ac4c151a98d
Bhakti261982 <bhaktinikam007@gmail.com>
2026-09-01 13:26:34 +0530

    Add files via upload

---
+++ lab9 ex1.py
@@ -0,0 +1,14 @@
+transactions = []
+
+for i in range(5):
+    amount = float(input("Enter transaction amount: "))
+    transactions.append(amount)
+
+
+largest = max(transactions)
+average = sum(transactions) / len(transactions)
+
+
+print("Largest transaction:", largest)
+print("Average spend:", average)
+
